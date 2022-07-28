bd835128a3608a7c1296251de0539a076d8f09e9
flash <flash@ip-172-31-3-10.ap-south-1.compute.internal>
2022-07-28 09:17:37 +0000

    add doctypes

---
+++ erp_hospitalrun/erp_hospitalrun/doctype/add_new/add_new.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Biswajit Sahoo and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Add New', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ erp_hospitalrun/erp_hospitalrun/doctype/test_ex_type/test_test_ex_type.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, Biswajit Sahoo and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestTestExType(unittest.TestCase):
+	pass
